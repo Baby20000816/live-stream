1641d27565a3494ad9f2ec160b45f51a996d6fe6
unknown <1273163614@qq.com>
2020-11-06 21:45:22 +0800

    管理员列表

--- app/router.js
+++ app/router.js
@@ -32,5 +32,8 @@ module.exports = (app) => {
 
   router.get('/test',controller.admin.test.page);
   router.get('/admin/manager/create',controller.admin.manager.create);
+ 
   router.post('/admin/manager',controller.admin.manager.save);
+
+  router.get('/admin/manager',controller.admin.manager.index);
 };
